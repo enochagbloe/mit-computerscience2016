78effeb7bd6fc364cbcce21750cf6d60667a334c
Enoch <your@email.com>
2026-02-24 05:33:16 +0000

    problem set 1

---
+++ problemSets/peoblemSet0/solution0.py
@@ -0,0 +1,6 @@
+import math
+numberX = input('Enter a number x: ')
+numberY = input('Enter a number y: ')
+solution = int(numberX) ** int(numberY)
+print("X**Y =", solution)
+print(math.log2(int(numberX)))
